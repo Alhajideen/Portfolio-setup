4b82960a55d2a1865b9b34b217238571fbc7e2a5
Alhajideen <salifunurudeen834@gmail.com>
2022-09-05 15:20:14 -0600

    add event listener on the link to navigate when clicked

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let hamburger = document.querySelector('.hamburger');
 let dropdown = document.querySelector('.mobile-dropdown');
 let closeX = document.querySelector('.close-x');
 let navigation = document.querySelector('.nav-logo');
+let menulink = document.querySelectorAll('.menulink');
 
 let buttonArr = [hamburger, closeX];
 buttonArr.forEach((e) => {
@@ -11,4 +12,13 @@ buttonArr.forEach((e) => {
     closeX.classList.toggle('visible');
     navigation.classList.toggle('noVisibility');
   });
+});
+
+menulink.forEach((e) => {
+  e.onclick = () => {
+    hamburger.classList.toggle('hidden');
+    dropdown.classList.toggle('visible');
+    closeX.classList.toggle('visible');
+    navigation.classList.toggle('noVisibility');
+  };
 });
