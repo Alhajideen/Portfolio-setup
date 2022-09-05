3d907de4c6c9c242fe19d183b4a984ae60477a22
jfoyarzo <foyarzo621@gmail.com>
2022-09-05 16:13:55 -0400

    Add classes for hidden and visible elements

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 let hamburger = document.querySelector('.hamburger');
 let dropdown = document.querySelector('.mobile-dropdown');
+let closeX = document.querySelector('.close-x');
 
-hamburger.addEventListener('click', () => {
-  
-});
+let buttonArr = [hamburger, closeX]
+buttonArr.forEach((e)=> {
+    e.addEventListener('click', () => {
+      
+    });
+
+})
