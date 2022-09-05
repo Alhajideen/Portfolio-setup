19f6614041310cd0f3312e97feb45a42c639f82d
Alhajideen <salifunurudeen834@gmail.com>
2022-09-05 15:09:53 -0600

    add css animation for the dropdown

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 let hamburger = document.querySelector('.hamburger');
 let dropdown = document.querySelector('.mobile-dropdown');
 let closeX = document.querySelector('.close-x');
+let navigation = document.querySelector('.nav-logo');
 
-let buttonArr = [hamburger, closeX]
-buttonArr.forEach((e)=> {
-    e.addEventListener('click', () => {
-      hamburger.classList.toggle('hidden')
-      dropdown.classList.toggle('visible')
-      closeX.classList.toggle('visible')
-    });
-
-})
+let buttonArr = [hamburger, closeX];
+buttonArr.forEach((e) => {
+  e.addEventListener('click', () => {
+    hamburger.classList.toggle('hidden');
+    dropdown.classList.toggle('visible');
+    closeX.classList.toggle('visible');
+    navigation.classList.toggle('noVisibility');
+  });
+});
